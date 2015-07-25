2fc523c123c45a3c1f86e891eb990fb60b956848
Hungriest Smurf <ceelveetza@gmail.com>
2015-07-24 21:02:13 -0700

    Create a database model for relationships - following and followers - and the migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ require 'bcrypt'
 
 class User < ActiveRecord::Base
   has_many :tweets
+  has_many :relationships, class_name:  "Relationship", foreign_key: "follower_id"
+  has_many :following, through: :relationships, source: :followed
+  
   include BCrypt
 
   def password
